5b083b45bbaea6a8e17f473205d3b7171c61805c
John <john@darla>
2014-09-04 12:16:06 -0400

    fucktastic

---
+++ headers/brownsys.h
@@ -0,0 +1,36 @@
+#include "proteins.h"
+#ifndef _BROWNSYS_H_
+#define _BROWNSYS_H_
+
+using namespace std;
+
+class brownsys
+{
+private:
+	protein main;
+	vector<protein> crowders;
+	bool crowds; 
+	int Ncr; //number of crowders
+public:
+	brownsys(); // creates a brownian system with one protein
+	brownsys(int num); // creates brownian system with num crowders
+	void something();
+	void startNNs(double cut); // at phi=0.2, 25<NNs<50
+	void updateNNs(double cut);
+	void moveall(mt19937& gen, normal_distribution<> distro);
+	void upall();
+	 
+};
+#endif
+
+
+/*NEED (in order)
+
+function to retrieve protein location
+
+
+
+
+
+
+*/
---
+++ test.cpp
@@ -0,0 +1,37 @@
+#include "headers/brownsys.h"
+
+int main(){
+	random_device rd; // Seed RNG
+	mt19937 gen(rd()); // start RNG
+	normal_distribution<> distro(0,pow(2*D*h,0.5));
+	brownsys baller(N);
+	baller.startNNs(4*r);
+	baller.something();
+	baller.moveall(gen,distro);
+	baller.upall(); // integrate with moveall eventually
+	baller.something();
+	baller.updateNNs(4*r);
+	
+	 
+	
+return 0;}
+
+/* SOME PSUEDO
+
+for the move-resolve-update stage
+
+- move serially (possibly randomize the order)
+- individually:
+-- check for collisions, resolve
+--- use NN list -> access other proteins in brownsys -> return their coords
+--- check if there are collisions and which happened first
+--- resolve earliest
+--- if main, check for exit or reaction
+--- check both particles involved (priority to main) for secondary collisions
+--- resolve and repeat until dt -> h
+-- check for periodic BCs, resolve
+-- update coordinates for all
+-- move on to next
+
+
+*/
